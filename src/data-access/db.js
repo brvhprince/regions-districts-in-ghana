b3da593193e9dd12bca47f697f144860348c74a5
Prince Takyi Akomea <prince.akomea.stu@uenr.edu.gh>
2023-08-08 17:36:40 +0000

    fixed bug where region doesn't exist

--- src/data-access/db.js
+++ src/data-access/db.js
@@ -11,6 +11,7 @@ export default function makeRegionsDb ({ makeDb }) {
         findByRegionId,
     })
     async function findAll () {
+
         const districts = makeDb.map(item=>item.districts.length).reduce((sum, a) => sum + a, 0)
         return {
             data: makeDb,
@@ -21,6 +22,15 @@ export default function makeRegionsDb ({ makeDb }) {
 
     async function findByRegionId ({ regionId }) {
         const results = makeDb.find((regions, _index) => regions.code === regionId.toUpperCase())
+
+        //check if region exists
+        if (!results) {
+            return {
+                data: [],
+                total_regions: 0,
+                total_districts: 0
+            }
+        }
         const districts = [results].map(item=>item.districts.length).reduce((sum, a) => sum + a, 0)
         return {
             data: results,
